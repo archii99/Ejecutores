cbf699733cd46569d717bc063a69c8597679daae
archii99 <64295475+archii99@users.noreply.github.com>
2020-12-02 22:58:58 -0300

    Update Requirements.sh

--- Requirements.sh
+++ Requirements.sh
@@ -1,2 +1,3 @@
 apt update && apt upgrade && pkg install curl && pkg install nmap && pkg install python &&  pkg install php && pkg install figlet 
-pkg install ruby && gem install ruby && pkg install lolcat
+pkg install ruby && gem install lolcat && pkg install lolcat
+pkg install wget
